16b4faa02cbb179fa1313a151e4d8ae440f9efa1
Luc Pascal Schroots <lucpschroots@hotmail.com>
2017-02-16 13:03:25 +0100

    Update 2.0

--- index.php
+++ index.php
@@ -1,27 +1,34 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Goede dag!</title>
-</head>
-<body>
-
 <?php 
 date_default_timezone_set("Europe/Amsterdam");
-$time = date("G:i");
+$time = date("H:i:s");
 
 	if ($hour >= "6" && $time <= "12"){
 		echo "Good Morning!";
 		$image = "backgrounds/morning.png"
-	} else if ($hour >= "12" && $time <= "18"){
+	else if ($hour >= "12" && $time <= "18"){
 		echo "Good Afternoon!";
 		$image = "backgrounds/afternoon.png"
-	} else if ($hour >= "18" && $time <= "23"){
+	} 
+	else if ($hour >= "18" && $time <= "23"){
 		echo "Good Evening!";
 		$image = "backgrounds/evening.png"
-	} else ($hour >= "23" && $time <= "6"){
+	} 
+	else ($hour >= "23" && $time <= "6"){
 		echo "Good Night!";
 		$image = "backgrounds/night.png"
 	}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welkom!</title>
+</head>
+<body
+
+<?php
+echo $time;
 ?>
 
 </body>
